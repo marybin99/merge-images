53918658eb3666a3fa050055c0436de0fda5c910
황수빈 <marybin99@naver.com>
2023-04-03 16:13:49 +0900

    axios 연결

--- src/ImageMerge.tsx
+++ src/ImageMerge.tsx
@@ -2,39 +2,12 @@ import React, { useEffect, useState } from "react";
 import mergeImages from "merge-images";
 import axios from "axios";
 
-function dataURLtoFile(dataurl: string, filename: string) {
-  var arr: any = dataurl.split(","),
-    mime = arr[0].match(/:(.*?);/)[1],
-    bstr = atob(arr[1]),
-    n = bstr.length,
-    u8arr = new Uint8Array(n);
-  while (n--) {
-    u8arr[n] = bstr.charCodeAt(n);
-  }
-  return new File([u8arr], filename, { type: mime });
-}
-
-const colorUrl =
-  "https://onthemars-dev.s3.ap-northeast-2.amazonaws.com/images/background-color/06.png";
-const cropUrl =
-  "https://onthemars-dev.s3.ap-northeast-2.amazonaws.com/images/vegi/01.png";
-const color = colorUrl.split("/"); // color[color.length-1] : 06.png
-const crop = cropUrl.split("/");
+// const colorUrl =
+//   "https://onthemars-dev.s3.ap-northeast-2.amazonaws.com/images/background-color/06.png";
+// const cropUrl =
+//   "https://onthemars-dev.s3.ap-northeast-2.amazonaws.com/images/vegi/01.png";
 
 function change(url, setImg) {
-  //   const token = sessionStorage.getItem("accessToken");
-  //   let xhr = new XMLHttpRequest();
-  //   //   xhr.open("GET", url, true);
-  //   if (typeof token === "string") xhr.setRequestHeader("Authorization", token);
-  //   xhr.responseType = "blob"; // 핵심
-  //   //   xhr.send();
-  //   xhr.onreadystatechange = function () {
-  //     if (this.readyState === 4 && this.status === 200) {
-  //       let url = window.URL || window.webkitURL;
-  //       let imgsrc = url.createObjectURL(this.response);
-  //       setImg(imgsrc);
-  //     }
-  //   };
   axios({
     method: "GET",
     url,
@@ -51,38 +24,33 @@ function ImageMerge() {
   const [img1, setImg1] = useState("");
   const [img2, setImg2] = useState("");
   const [mergeImgSrc, SetMergeImgSrc] = useState();
+
+  const baseUrl = "https://j8e207.p.ssafy.io/api/v1";
+  const dna = 1020200000000000000;
+  const [colorUrl, setColorUrl] = useState("");
+  const [cropUrl, setCropUrl] = useState("");
+  
+  useEffect(() => {
+    axios.get(baseUrl + `/farm/mint/${dna}`).then((res) => {
+      setColorUrl(res.data.colorUrl);
+      setCropUrl(res.data.cropUrl);
+    });
+  });
+
   useEffect(() => {
     change(colorUrl, setImg1);
     change(cropUrl, setImg2);
   }, []);
 
-  //   const merge = async () =>
-  //     await mergeImages(
-  //       [
-  //         { src: img1, x: 0, y: 0 },
-  //         { src: img2, x: 5, y: 0 },
-  //       ],
-  //       { width: 128, height: 128 }
-  //     )
-  //       .then((b64: any) => {
-  //         console.log("b64 : " + b64);
-  //         SetMergeImgSrc(b64);
-  //       })
-  //       .catch((e) => console.log(e.message));
-
   useEffect(() => {
     // merge();
-    mergeImages(
-      [
-          { src: img1, x: 0, y: 0 },
-          { src: img2, x: 5, y: 0 },
-      ],
-      { width: 128, height: 128 }
-    )
-      .then((b64: any) => {
-        console.log("b64 : " + b64);
-        SetMergeImgSrc(b64);
-      })
+    mergeImages([
+      { src: img1, x: 0, y: 0 },
+      { src: img2, x: 0, y: 0 },
+    ]).then((b64: any) => {
+      console.log("b64 : " + b64);
+      SetMergeImgSrc(b64);
+    });
   }, [mergeImgSrc, img1, img2]);
 
   return (
@@ -112,6 +80,7 @@ export default ImageMerge;
 
 const style = {
   marginTop: "200px",
+  marginBottom: "100px",
 };
 
 const imageStyle = {
